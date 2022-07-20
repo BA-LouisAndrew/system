8d4300034066d0455c45b4c9bc412056a398df30
Louis Andrew <louisandrew3@gmail.com>
2022-07-20 17:17:37 +0200

    hotfix(clone-script): clone services repo

--- clone-components.sh
+++ clone-components.sh
@@ -2,6 +2,8 @@
 
 rmdir ui 
 rmdir fds
+rmdir services
 
 git clone https://github.com/BA-LouisAndrew/ui.git
 git clone https://github.com/BA-LouisAndrew/fds.git
+git clone https://github.com/BA-LouisAndrew/services.git
